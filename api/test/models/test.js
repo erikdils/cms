f278d9cd0d292f6eb5ee43490d2d047d1f8137c3
erikdils <erikdils@gmail.com>
2021-05-31 00:24:27 +0200

    update strapi

--- api/test/models/test.js
+++ api/test/models/test.js
@@ -1,8 +1,33 @@
-'use strict';
+"use strict";
+const axios = require("axios");
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+  afterCreate: async (entry) => {
+    axios
+      .post(strapi.config.currentEnvironment.staticWebsiteBuildURL, entry)
+      .catch(() => {
+        // Ignore
+      });
+  },
+
+  afterUpdate: async (entry) => {
+    axios
+      .post(strapi.config.currentEnvironment.staticWebsiteBuildURL, entry)
+      .catch(() => {
+        // Ignore
+      });
+  },
+
+  afterDestroy: async (entry) => {
+    axios
+      .post(strapi.config.currentEnvironment.staticWebsiteBuildURL, entry)
+      .catch(() => {
+        // Ignore
+      });
+  },
+};
